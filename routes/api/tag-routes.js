1140cdf3b8e31b7f58059af94ed9568156159e9d
oli-drew <89919025+oli-drew@users.noreply.github.com>
2022-03-22 15:27:00 +0000

    Update tag working

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -46,8 +46,22 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.put("/:id", (req, res) => {
-  // update a tag's name by its `id` value
+// Update a tag by its `id` value
+router.put("/:id", async (req, res) => {
+  try {
+    const tagData = await Tag.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!tagData) {
+      res.status(404).json({ message: "No tag with this id!" });
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // Delete a tag by its `id` value
