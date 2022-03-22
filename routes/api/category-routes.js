c336f85415cfbfce63c407366def71cd0b5ea9af
oli-drew <89919025+oli-drew@users.noreply.github.com>
2022-03-22 15:21:25 +0000

    Update category working

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -46,8 +46,22 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.put("/:id", (req, res) => {
-  // update a category by its `id` value
+// Update a category by its `id` value
+router.put("/:id", async (req, res) => {
+  try {
+    const categoryData = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: "No category with this id!" });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // Delete a category by its `id` value
