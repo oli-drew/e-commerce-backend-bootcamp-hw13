ea34b2ff5038e082a731f59a0c1deaa59188c0e1
oli-drew <89919025+oli-drew@users.noreply.github.com>
2022-03-22 12:50:34 +0000

    Added category model columns

--- models/index.js
+++ models/index.js
@@ -1,8 +1,8 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
 
@@ -18,3 +18,35 @@ module.exports = {
   Tag,
   ProductTag,
 };
+
+//
+// Reader.hasOne(LibraryCard, {
+//   foreignKey: 'reader_id',
+//   // When we delete a Reader, make sure to also delete the associated Library Card.
+//   onDelete: 'CASCADE',
+// });
+
+// LibraryCard.belongsTo(Reader, {
+//   foreignKey: 'reader_id',
+// });
+
+// //
+// Traveller.belongsToMany(Location, {
+//   // Define the third table needed to store the foreign keys
+//   through: {
+//     model: Trip,
+//     unique: false
+//   },
+//   // Define an alias for when data is retrieved
+//   as: 'planned_trips'
+// });
+
+// Location.belongsToMany(Traveller, {
+//   // Define the third table needed to store the foreign keys
+//   through: {
+//     model: Trip,
+//     unique: false
+//   },
+//   // Define an alias for when data is retrieved
+//   as: 'location_travellers'
+// });
